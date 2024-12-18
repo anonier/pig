0432bd77431492b8653a6292d05d405c0f685ba3
paul <7632722@gmail.com>
2024-12-18 16:06:14 +0800

    屏蔽mqttclient连接

--- pig-upms/pig-upms-biz/src/main/java/com/pig4cloud/pig/admin/iot/mq/IMqttController.java
+++ pig-upms/pig-upms-biz/src/main/java/com/pig4cloud/pig/admin/iot/mq/IMqttController.java
@@ -3,7 +3,6 @@ package com.pig4cloud.pig.admin.iot.mq;
 
 import com.pig4cloud.pig.common.core.util.R;
 import lombok.RequiredArgsConstructor;
-import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,38 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class IMqttController {
 
-    private final MqttClient myMqttClient;
+//    private final MqttClient myMqttClient;
 
-    @PostMapping("/send")
-    public R<Object> send(String topic, String msg) {
-        try {
-            MqttMessage message = new MqttMessage(msg.getBytes());
-            message.setQos(1);
-            myMqttClient.publish(topic, message);
-            return R.ok();
-        } catch (Exception e) {
-            return R.failed();
-        }
-    }
+	@PostMapping("/send")
+	public R<Object> send(String topic, String msg) {
+		try {
+			MqttMessage message = new MqttMessage(msg.getBytes());
+			message.setQos(1);
+//            myMqttClient.publish(topic, message);
+			return R.ok();
+		} catch (Exception e) {
+			return R.failed();
+		}
+	}
 
-    /**
-     * MQ4IoT支持点对点消息，即如果发送方明确知道该消息只需要给特定的一个设备接收，且知道对端的 clientId，则可以直接发送点对点消息。
-     * 点对点消息不需要经过订阅关系匹配，可以简化订阅方的逻辑。点对点消息的 topic 格式规范是  {{parentTopic}}/p2p/{{targetClientId}}
-     *
-     * @param topic
-     * @param msg
-     * @param clientId
-     * @return
-     */
-    @PostMapping("/sendP2p")
-    public R<Object> sendP2p(String topic, String msg, Long clientId) {
-        try {
-            MqttMessage message = new MqttMessage(msg.getBytes());
-            message.setQos(1);
-            myMqttClient.publish(topic + "/p2p/" + clientId, message);
-            return R.ok();
-        } catch (Exception e) {
-            return R.failed();
-        }
-    }
+	/**
+	 * MQ4IoT支持点对点消息，即如果发送方明确知道该消息只需要给特定的一个设备接收，且知道对端的 clientId，则可以直接发送点对点消息。
+	 * 点对点消息不需要经过订阅关系匹配，可以简化订阅方的逻辑。点对点消息的 topic 格式规范是  {{parentTopic}}/p2p/{{targetClientId}}
+	 *
+	 * @param topic
+	 * @param msg
+	 * @param clientId
+	 * @return
+	 */
+	@PostMapping("/sendP2p")
+	public R<Object> sendP2p(String topic, String msg, Long clientId) {
+		try {
+			MqttMessage message = new MqttMessage(msg.getBytes());
+			message.setQos(1);
+//            myMqttClient.publish(topic + "/p2p/" + clientId, message);
+			return R.ok();
+		} catch (Exception e) {
+			return R.failed();
+		}
+	}
 }
